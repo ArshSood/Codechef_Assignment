526c1140d1d10b8629323c4edbf09476dcb39b86
ArshSood <arshsood04@gmail.com>
2021-03-12 03:49:58 +0530

    git@github.com:ArshSood/Codechef_Assignment.git

--- PEC2021G/PWRADD.py
+++ PEC2021G/PWRADD.py
@@ -10,14 +10,28 @@ def add(a, b):
 # function defined above.
  
 # Note that this function should return (a ** b) % m, you are guaranteed that add(a, b) returns (a + b) % m
- 
+
 def pwr(a, b):
-    return add(a**b,0)
+    
+    #M=add(a,0)
+    #counter=M
+    y=1
+    for i in range(0,b):
+        counter=0
+        for j in range(0,a):
+            counter=add(counter,y)
+        y=counter
+       
+        #counter*=M
+        #counter=add(M*counter,0)
+        
+    return y
  
- 
+
 # --------------------- Do not touch anything below this line ----------------------
  
 a, b = map(int, input().split())
 ans = pwr(a, b)
 print('2', ans)
+ 
  
